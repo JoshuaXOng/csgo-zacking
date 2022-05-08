6460d417e8d229620f9de1837d3cff56d0a425fd
JoshuaXOng <joshuaong2000@gmail.com>
2022-05-08 22:28:07 +1000

    feat: added demo of set view angle

--- src/main.cpp
+++ src/main.cpp
@@ -16,20 +16,22 @@
 
 struct Module {
   HANDLE process;
-  uintptr_t module;
+  uintptr_t client_module;
+  uintptr_t engine_module;
 };
 
-Module get_csgo_client_module() {
+Module get_csgo_client_and_engine_module() {
   DWORD csgo_id = get_process_id(L"csgo.exe");
   HANDLE csgo = OpenProcess(PROCESS_ALL_ACCESS, false, csgo_id);
   uintptr_t client_dll = get_module_base_address(csgo_id, L"client.dll");
-  return { csgo, client_dll };
+  uintptr_t engine_dll = get_module_base_address(csgo_id, L"engine.dll");
+  return { csgo, client_dll, engine_dll };
 }
 
 int wWinMain(HINSTANCE h_instance, HINSTANCE h_orev_instance, LPWSTR p_cmd_line, int n_cmd_show) {
-  auto [csgo, client_dll] = get_csgo_client_module();
-  if (csgo == NULL || client_dll == 0) {
-    std::cout << "Could not get CSGO client module" << std::endl;
+  auto [csgo, client_dll, engine_dll] = get_csgo_client_and_engine_module();
+  if (csgo == NULL || client_dll == 0 || engine_dll == 0) {
+    std::cout << "Could not get CSGO client or engine module" << std::endl;
     return EXIT_FAILURE;
   }
 
@@ -45,13 +47,23 @@ int wWinMain(HINSTANCE h_instance, HINSTANCE h_orev_instance, LPWSTR p_cmd_line,
   while (true) {
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-    std::cout << "At" << " " << get_number_as_hex(health_reader->get_value().address) << ": " << health_reader->get_value().value << std::endl;
+    // std::cout << "At" << " " << get_number_as_hex(health_reader->get_value().address) << ": " << health_reader->get_value().value << std::endl;
 
-    std::cout << idk_entity_health_reader->get_value().value << std::endl;
+    // std::cout << idk_entity_health_reader->get_value().value << std::endl;
     // std::cout << idk_entity_vec_origin_reader->get_value().value << std::endl;
-    std::cout << idk_entity_vec_origin_reader->get_value_as_3d_vector().x << std::endl;
-    std::cout << idk_entity_vec_origin_reader->get_value_as_3d_vector().y << std::endl;
-    std::cout << idk_entity_vec_origin_reader->get_value_as_3d_vector().z << std::endl;
+    // std::cout << idk_entity_vec_origin_reader->get_value_as_3d_vector().x << std::endl;
+    // std::cout << idk_entity_vec_origin_reader->get_value_as_3d_vector().y << std::endl;
+    // std::cout << idk_entity_vec_origin_reader->get_value_as_3d_vector().z << std::endl;
+
+    // Vec3 a;
+    // ReadProcessMemory(csgo, (BYTE*) follow_pointer_chain(csgo, engine_dll + 0x58CFC4, { 0x4D90 }), &a, sizeof(Vec3), 0);
+    // std::cout << a.x << std::endl;
+    // std::cout << a.y << std::endl;
+    // std::cout << a.z << std::endl;
+    
+    Vec3 b = { 1., 150., 0 };
+    bool result = WriteProcessMemory(csgo, (BYTE*) follow_pointer_chain(csgo, engine_dll + 0x58CFC4, { 0x4D90 }), &b, sizeof(Vec3), 0);
+    std::cout << result << std::endl;
   };
 
   return EXIT_SUCCESS;
